4f6125adafc477ede6f291085846baec50e56c34
neev <neevsharma828@gmail.com>
2025-05-14 16:46:23 +0530

    second commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,20 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Navbar.css';
 import logo from '../assets/logo.png';
-import { FaSearch, FaQuestionCircle, FaUser, FaMapMarkerAlt } from 'react-icons/fa';
+import { FaSearch, FaQuestionCircle, FaUser, FaMapMarkerAlt, FaBars, FaTimes } from 'react-icons/fa';
 
 const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const navItems = [
+    'Anniversary',
+    'Birthdays',
+    'Gifts',
+    'Candlelight Dinners',
+    'Decorations',
+    'Festivals',
+    "Kid's Celebrations"
+  ];
+
   return (
     <div className="navbar-wrapper">
       <div className="top-bar">
         <div className="logo">
           <img src={logo} alt="CherishX" />
-    
         </div>
+
         <div className="search-bar">
           <input type="text" placeholder="What are you celebrating?" />
           <FaSearch className="search-icon" />
         </div>
+
+        <div className="hamburger" onClick={() => setMenuOpen(!menuOpen)}>
+          {menuOpen ? <FaTimes /> : <FaBars />}
+        </div>
+
         <div className="nav-icons">
           <div className="icon-block"><FaQuestionCircle /><span>HELP CENTER</span></div>
           <div className="icon-block"><FaUser /><span>LOG IN</span></div>
@@ -25,8 +42,8 @@ const Navbar = () => {
         </div>
       </div>
 
-      <div className="bottom-bar">
-        {['Anniversary', 'Birthdays', 'Gifts', 'Candlelight Dinners', 'Decorations', 'Festivals', "Kid's Celebrations"].map(item => (
+      <div className={`bottom-bar ${menuOpen ? 'open' : ''}`}>
+        {navItems.map(item => (
           <div key={item} className="nav-item">
             {item} <span className="dropdown">▼</span>
           </div>
